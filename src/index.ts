bc92248155342374c4498efa6f4ec4c625b6c2e8
Voltrex <mohammadkeyvanzade94@gmail.com>
2021-09-13 20:56:40 +0430

    refactor(src): fix formatting
    Fixed the jumbled up formatting.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import Dexter from './client'
+import Dexter from './client.ts';
 
-const client = new Dexter("Moon")
-client.train({"hello": "sup"})
-console.log(client)
+const client = new Dexter('Moon');
+
+client.train({ hello: 'sup' });
+
+console.log(client);
 console.log(client.answer);
-console.log(client.answer("hello"));
+console.log(client.answer('hello'));
 console.log(client.correct_answer);
-console.log(client.name)
-console.log(client.answer("Test",{debug: true}))
+console.log(client.name);
+console.log(client.answer('Test', { debug: true }));
